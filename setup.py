ad9328c01cb8e31291b33b1312a1efc79b31092f
Adam Hare <adamth63@gmail.com>
2020-06-02 16:07:00 -0400

    Update setup.py
    Added allennlp as requirement

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['tqdm', 'requests', 'numpy'],
+    install_requires=['tqdm', 'requests', 'numpy', 'allennlp'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
